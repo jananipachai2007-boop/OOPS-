41eb1ac6608fd291749e5c700063a0c20afdecf2
jananipachai2007-boop <jananipachai2007@gmail.com>
2026-08-19 10:28:51 +0530

    Add files via upload

---
+++ Volume.java
@@ -0,0 +1,10 @@
+java conversion;
+public class Volume{
+  public double litreToMillilitre(double litre){
+    return litre*1000;
+  }
+  public double millilitreToLitre(double ml){ 
+    return ml/1000;
+  }
+}
+
